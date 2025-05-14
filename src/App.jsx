c1de0ba10db99d7f7e7d5fe4a88a0d99d7f1ad7c
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-13 21:02:49 -0500

    removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,15 @@
 import React from "react";
 import { CourseProvider } from "./CourseContext";
 import Header from "./components/Header";
-import Section from "./components/Section";
-import HelpfulResource from "./components/HelpfulResource";
-import SchoolCatalog from "./SchoolCatalog"; // Ensure this import is correct
-import ClassSchedule from "./ClassSchedule"; // Ensure this import is correct
-import Footer from "./components/Footer"; // Ensure this import is correct
+import SchoolCatalog from "./SchoolCatalog";
+import ClassSchedule from "./ClassSchedule";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <CourseProvider>
       <div className="App">
         <Header />
-        <Section title="Helpful resources">
-          <HelpfulResource
-            label="React Official Docs"
-            link="https://reactjs.org/docs/getting-started.html"
-          />
-          <HelpfulResource
-            label="ReactJS Tutorial - freeCodeCamp"
-            link="https://www.youtube.com/watch?v=bMknfKXIFA8"
-          />
-          <HelpfulResource
-            label="React Docs Beta (New)"
-            link="https://beta.reactjs.org/"
-          />
-        </Section>
         <SchoolCatalog />
         <ClassSchedule />
         <Footer />
