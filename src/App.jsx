cbeb2da8307a21fb9aefb8da33bc995f395d7aa4
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-13 19:50:25 -0500

    wrap my application with the CourseProvider to make the context available throughout your component tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import SchoolCatalog from "./SchoolCatalog";
-import Header from "./Header";
-import ClassSchedule from "./ClassSchedule";
+// src/App.jsx
+import React from "react";
+import { CourseProvider } from "./CourseContext";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import HelpfulResource from "./components/HelpfulResource";
+import SchoolCatalog from "./SchoolCatalog"; // Ensure this import is correct
+import ClassSchedule from "./ClassSchedule"; // Ensure this import is correct
+import Footer from "./components/Footer"; // Ensure this import is correct
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Header />
-      <SchoolCatalog />
-      <ClassSchedule />
-    </div>
+    <CourseProvider>
+      <div className="App">
+        <Header />
+        <Section title="Helpful resources">
+          <HelpfulResource
+            label="React Official Docs"
+            link="https://reactjs.org/docs/getting-started.html"
+          />
+          <HelpfulResource
+            label="ReactJS Tutorial - freeCodeCamp"
+            link="https://www.youtube.com/watch?v=bMknfKXIFA8"
+          />
+          <HelpfulResource
+            label="React Docs Beta (New)"
+            link="https://beta.reactjs.org/"
+          />
+        </Section>
+        <SchoolCatalog />
+        <ClassSchedule />
+        <Footer />
+      </div>
+    </CourseProvider>
   );
 }
+
+export default App;
