f087958aba5c34f0cdcb89b0b6418a15c52a5658
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-13 20:20:41 -0500

    created a helpful resource

---
+++ src/components/HelpfulResource.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OpenLink from "./OpenLink";
+import CopyLink from "./CopyLink";
+
+export default function HelpfulResource({ link, label }) {
+  return (
+    <div className="helpful-resource">
+      {label}
+      <OpenLink link={link} />
+      <CopyLink link={link} />
+    </div>
+  );
+}
